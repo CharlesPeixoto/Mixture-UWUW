081df7abc720d1e54adafc1641ef1750fb2e3532
CharlesPeixoto <72995287+CharlesPeixoto@users.noreply.github.com>
2020-10-16 15:44:20 -0300

    Update Section_2_map.R

--- Section_2_map.R
+++ Section_2_map.R
@@ -2,25 +2,25 @@ rm(list=ls())
 library(tidyverse)
 library(brazilmaps)
 library(readr)
-setwd("~/rrg1@de.ufpe.br/Bruce/R/Dados Brutos/pre processamento 2018")
-
-
-
 library(readxl)
-CODIGOS <- read_excel("CODIGOS.xlsx") 
+library(sf)
 
-### mapa dos municipios onde o bolonaro ganha
+CODIGOS <- read_csv("CODIGOS.csv") 
+
 dados<-read_csv("dados_2018_2turno_BRANCOSeNULOS.csv") %>% 
   mutate(PROP_PSL=`JAIR BOLSONARO`/VOTOS_NOMINAIS) %>%
-  mutate(PROP_PSL_BN=`JAIR BOLSONARO`/(VOTOS_NOMINAIS+QT_VOTOS_BRANCOS+QT_VOTOS_NULOS)) %>%
-  mutate(des2018 =  case_when(0 < PROP_PSL  & PROP_PSL < .4999999 ~ "Votos PT",
-                              .5 < PROP_PSL  & PROP_PSL  < 1 ~ "Votos PSL")) 
-
+  mutate(PROP_PSL_BN=`JAIR BOLSONARO`/(VOTOS_NOMINAIS + QT_VOTOS_BRANCOS + QT_VOTOS_NULOS)) %>%
+  mutate(des2018 =  case_when(0 < PROP_PSL  & PROP_PSL < .4999999 ~ "Fernando Haddad",
+                              .5 < PROP_PSL  & PROP_PSL  < 1 ~ "Jair Bolsonaro")) 
 
 BR<-get_brmap("City")
 
-BR %>% left_join(dados, c("City" = "codigo_ibge")) %>%
+map2018 <- BR %>% left_join(dados, c("City" = "codigo_ibge")) %>%
   ggplot() +
-  geom_sf(aes(fill = des2018), size = 0.01) +  
-  scale_fill_manual(name = "% Votos PSL",values = c("lightblue","red")) +
+  geom_sf(aes(fill = des2018), size = 0.00000001) +  
+  scale_fill_manual(name = "Winning candidate", values = c("blue","orange")) +
   theme(panel.background = element_rect(fill = "white", colour = "white"))
+
+map2018
+
+
