d7c0729de30695f2e4c1b96465de05cfe7dc7ef8
CharlesPeixoto <72995287+CharlesPeixoto@users.noreply.github.com>
2020-10-19 14:46:04 -0300

    Update and rename Section_4_graphic_UWUW_Repa.R to Section_4_UWUW_graphic_Repa.R

--- Section_4_UWUW_graphic_Repa.R
+++ Section_4_UWUW_graphic_Repa.R
@@ -21,6 +21,8 @@ f_UWUW<- function(y)
   }
 ###############################################################################
 # Information for the construction of Figure 4a of Section 4.
+yliminf=0
+ylimsup=4.5
 
 l1 = 0.2
 l2 = 0.1
@@ -69,6 +71,8 @@ legend("topright", c(expression(paste(mu,1, " = 0.2, ", mu,2, " = 0.1, ", beta,2
 ###############################################################################
 ###############################################################################
 # Information for the construction of Figure 4b of Section 4.
+yliminf=0
+ylimsup=3.5
 
 l1 = 0.3
 l2 = 0.1
@@ -119,6 +123,8 @@ legend("topright", c(expression(paste(mu,1, " = 0.3, ", mu,2, " = 0.1, ", beta,2
 ###############################################################################
 ###############################################################################
 # Information for the construction of Figure 4c of Section 4.
+yliminf=0
+ylimsup=6
 
 l1 = 0.1
 l2 = 0.2
@@ -170,52 +176,53 @@ legend("topright", c(expression(paste(mu,1, " = 0.1, ", mu,2, " = 0.2, ", beta,1
 ###############################################################################
 ###############################################################################
 # Information for the construction of Figure 4d of Section 4.
+yliminf=0
+ylimsup=5
 
-l1 = 0.2
-l2 = 0.1
-b1 = 2.5
-b2 = 2.0
+l1 = 0.6
+l2 = 0.2
+b1 = 4.0
+b2 = 3.0
 curve(f_UWUW,from=fromx, to=tox, add = FALSE, lty= 1,xlab = expression("y"),
       ylab = expression("f(y)"),ylim =c(yliminf,ylimsup), col =1, lwd = 1) 
 
-l1 = 0.4
-l2 = 0.2
+l1 = 0.7
+l2 = 0.3
 b1 = 3.0
-b2 = 2.0
+b2 = 5.0
 curve(f_UWUW,from=fromx, to=tox, add = TRUE, lty=1,
       ylab = expression("f(y)"),ylim =c(yliminf,ylimsup), col = 2, lwd = 1)
 
-l1 = 0.5
-l2 = 0.3
-b1 = 3.1
-b2 = 2.5
+l1 = 0.8
+l2 = 0.4
+b1 = 4.0
+b2 = 5.5
 curve(f_UWUW,from=fromx, to=tox, add = TRUE, lty=1,
       ylab = expression("f(y)"),ylim =c(yliminf,ylimsup), col = 3, lwd = 1)
 
-l1 = 0.6
-l2 = 0.4
-b1 = 4.0
-b2 = 5.0
+l1 = 0.9
+l2 = 0.5
+b1 = 2.0
+b2 = 4.0
 curve(f_UWUW,from=fromx, to=tox, add = TRUE, lty=1, ylab = expression("f(y)"),
       ylim =c(yliminf,ylimsup), col = 4, lwd = 1)
 
 l1 = 0.7
 l2 = 0.5
 b1 = 5.0
-b2 = 5.0
+b2 = 6.0
 curve(f_UWUW,from=fromx, to=tox, add = TRUE, lty=1,
       ylab = expression("f(y)"),xlab = expression("y"),
       ylim =c(yliminf,ylimsup), col = 5, lwd = 1)
 
 
-legend("topright", c(expression(paste(mu,1, " = 0.2, ", mu,2, " = 0.1, ", beta,1,' = 2.5, ', beta,2,' = 2.0 ')),
-                     c(expression(paste(mu,1, " = 0.4, ", mu,2, " = 0.2, ", beta,1,' = 3.0, ', beta,2,' = 2.0 ')),
-                       c(expression(paste(mu,1, " = 0.5, ", mu,2, " = 0.3, ", beta,1,' = 3.1, ', beta,2,' = 2.5 ')),
-                         c(expression(paste(mu,1, " = 0.6, ", mu,2, " = 0.4, ", beta,1,' = 4.0, ', beta,2,' = 5.0 ')),
-                           c(expression(paste(mu,1, " = 0.7, ", mu,2, " = 0.5, ", beta,1,' = 5.0, ', beta,2,' = 5.0'))))))),
+legend("topright", c(expression(paste(mu,1, " = 0.6, ", mu,2, " = 0.2, ", beta,1,' = 4.0, ', beta,2,' = 3.0 ')),
+                     c(expression(paste(mu,1, " = 0.7, ", mu,2, " = 0.3, ", beta,1,' = 3.0, ', beta,2,' = 5.0 ')),
+                       c(expression(paste(mu,1, " = 0.8, ", mu,2, " = 0.4, ", beta,1,' = 4.0, ', beta,2,' = 5.5 ')),
+                         c(expression(paste(mu,1, " = 0.9, ", mu,2, " = 0.5, ", beta,1,' = 2.0, ', beta,2,' = 4.0 ')),
+                           c(expression(paste(mu,1, " = 0.7, ", mu,2, " = 0.5, ", beta,1,' = 5.0, ', beta,2,' = 6.0'))))))),
        col = c(1,2,3,4,5),
        lty= c(1,1,1,1,1),
        lwd = c(1,1,1,1,1), bty="n", cex = 0.8)
-
 
 
