9986ea9c3e1d94902aa88c9663d30c19cdde3831
CharlesPeixoto <72995287+CharlesPeixoto@users.noreply.github.com>
2020-10-19 18:12:06 -0300

    Update Section_4_UWUW_properties.R

--- Section_4_UWUW_properties.R
+++ Section_4_UWUW_properties.R
@@ -1,10 +1,11 @@
 ##Checking the moments of the two-component unit Weibull mixture model
+library("gsl")
 ####################################
 tau = 0.5 
-mu1 = 0.6
-mu2 = 0.5
-beta1 = 1.5
-beta2 = 1.3
+mu1 = 0.1
+mu2 = 0.8
+beta1 = 3
+beta2 = 2
 p = 0.4
 ####################################################
 ### Creating a function to calculate density ###
@@ -26,56 +27,57 @@ F_UWUW<-function(x){
 y=.9
 integrate(f_UWUW,0,y)
 F_UWUW(y)
+ 
 
-# h-th numerical moment
+# First analytical moment
+R = 100
+vec= vector()
+vec1 = vector()
+k = 0
 s = 5
 mh_UWUW<-function(x){
   x^s*f_UWUW(x)
 }
 
-# First analytical moment
-R = 15
-mu = vector()
-beta = vector()
-k = 0
-d = p* beta1*((-log(tau)/(-log(mu1))^beta1))
-g = (1-p)* beta2*((-log(tau)/(-log(mu2))^beta2))
-
 for( k in 0:R){
-    mu[k+1] = (1/factorial(k))*(((log(tau))^k)/((-log(mu1))^(k*beta1)))*
-      s^(-beta1*(k+1))*gamma(beta1*(k+1))
+  vec[k+1] = ((s*log(mu1))^k)/(factorial(k)*(-log(tau))^(k/beta1))*
+    gamma((k/beta1 )+1)
   }
 
 for( k in 0:R){
-  beta[k+1] = (1/factorial(k))*(((log(tau))^k)/((-log(mu2))^(k*beta2)))*
-    s^(-beta2*(k+1))*gamma(beta2*(k+1))
+  vec1[k+1] = ((s*log(mu2))^k)/(factorial(k)*(-log(tau))^(k/beta2))*
+    gamma((k/beta2)+1)
 }
 
-q = d*sum(mu) + g*sum(beta)
+q = p*sum(vec) + (1-p)*sum(vec1)
 q   ## moment of the two-component unit Weibull mixture model
 
 integrate(mh_UWUW,0,1)
 
-
 # sth  numerical incomplete moment
-r = 0.9
+r = 0.8
 mhi_UWUW<-function(x){
   x^s*f_UWUW(x)
 }
-muu = vector()
-betaa = vector()
+
+vec2 = vector()
+vec3 = vector()
+
+a = - log(tau)/(-log(mu1))^beta1
+g  =  - log(tau)/(-log(mu2))^beta2
+
 # h-th analytical moment
 for( k in 0:R){
-  muu[k+1] = (1/factorial(k))*(((log(tau))^k)/((-log(mu1))^(k*beta1)))*
-    s^(-beta1*(k+1))*pgamma(beta1*(k+1), - s*log(r))
+  vec2[k+1] = (-s)^k*a^(-k/beta1)*
+    (gamma_inc(k/beta1 + 1, a*(-log(r))^beta1))/factorial(k)
 }
 
 for( k in 0:R){
-  betaa[k+1] = (1/factorial(k))*(((log(tau))^k)/((-log(mu2))^(k*beta2)))*
-    s^(-beta2*(k+1))*pgamma(beta2*(k+1), - s*log(r))
+  vec3[k+1] = (-s)^k*g^(-k/beta2)*
+    (gamma_inc(k/beta2 + 1, g*(-log(r))^beta2))/factorial(k)
 }
 
-h = d*sum(muu) + g*sum(betaa)
+h = p*sum(vec2) + (1-p)*sum(vec3)
 h   ## sth moment of the two-component unit Weibull mixture model
 
 integrate(mhi_UWUW,0,r)
